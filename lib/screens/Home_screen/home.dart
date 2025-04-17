7372012304bf7f1afd27ee3f472488dbb5a9be2e
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2025-04-17 21:14:28 +0600

    update

--- lib/screens/Home_screen/home.dart
+++ lib/screens/Home_screen/home.dart
@@ -12,7 +12,8 @@ class Home extends StatefulWidget {
 }
 
 final allprodact = product;
-
+  List<String> tabs = ["Hottest", "Popular", "New", "Combo", "Top"];
+  int selectedIndex = 0;
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
@@ -46,115 +47,156 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Text('Hello Tony, What fruit salad'),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Text('Combo do you want today?'),
-          ),
-          SizedBox(
-            height: 24,
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 21),
-                child: Container(
-                  width: wight * 0.705,
-                  height: 56,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: Color(0xfff3f4f9),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        label: Row(
-                          children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 24),
-                              child: Container(
-                                height: 15,
-                                width: 15,
-                                child: Image.asset(
-                                  'assets/images/bi_search.png',
-                                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text('Hello Tony, What fruit salad'),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text('Combo do you want today?'),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 21),
+                  child: Container(
+                    width: wight * 0.705,
+                    height: 56,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Color(0xfff3f4f9),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          label: Row(
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 24),
+                                child: Container(
+                                  height: 15,
+                                  width: 15,
+                                  child: Image.asset(
+                                    'assets/images/bi_search.png',
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                               ),
-                            ),
-                            Text("Search for fruit salad combos",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w400,
-                                ))
-                          ],
-                        )),
+                              Text("Search for fruit salad combos",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                  ))
+                            ],
+                          )),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Container(
-                width: 26,
-                height: 17,
-                child: Image.asset(
-                  'assets/images/menu2.png',
-                  fit: BoxFit.cover,
+                SizedBox(
+                  width: 10,
                 ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 40),
-            child: Text("Recommended Combo",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w500,
-                )),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              foodcard(
-                allprodact[0].img!,allprodact[0].name!,
-                allprodact[0].pric!,
-                
-              ),
-              foodcard(allprodact[1].img!, allprodact[1].name!,
-              allprodact[1].pric!,
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 48,
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              spacing: 50,
-              children: [
-                Chip(
-                    label: Text('Hottest'),
-                    backgroundColor: Colors.orange[100]),
-                Chip(label: Text('')),
-                Chip(label: Text('New combo')),
-                Chip(label: Text('Top')),
+                Container(
+                  width: 26,
+                  height: 17,
+                  child: Image.asset(
+                    'assets/images/menu2.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ],
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 40),
+              child: Text("Recommended Combo",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  )),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                foodcard(
+                  allprodact[0].img!,allprodact[0].name!,
+                  allprodact[0].pric!,context
+                  
+                ),
+                foodcard(allprodact[1].img!, allprodact[1].name!,
+                allprodact[1].pric!,context
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48,
+            ),
+          Container(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: List.generate(tabs.length, (index) {
+            bool isSelected = selectedIndex == index;
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    
+                    child: Text(
+                      tabs[index],
+                      style: TextStyle(
+                        fontSize: isSelected? 24 :16,
+                        fontWeight: isSelected ? FontWeight.w500 : FontWeight.normal,
+                        color: isSelected ? Color(0xff27214D) : Color(0xff938DB5),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Container(
+                    height: 3,
+                    width: 22,
+                    decoration: BoxDecoration(
+                      color: isSelected ? Color(0xfffFFA451) : Colors.transparent,
+                     
+                    ),
+                  )
+                ],
+              ),
+            );
+          }),
+        ),
+            ),
+            SizedBox(height: 24,),
+            SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+            spacing: 16,
+          children: [
+           rowfoodcard(allprodact[2].img!,allprodact[2].name! , allprodact[2].pric!, context ,0xfffff9ea ),   
+            rowfoodcard(allprodact[3].img!,allprodact[3].name! , allprodact[3].pric!, context, 0xfffef0f0 ), 
+            rowfoodcard(allprodact[4].img!,allprodact[4].name! , allprodact[4].pric!, context , 0xfff1eff6),
+        ],),
+            )
+        
+          ],
+        ),
       ),
     );
   }
--- lib/screens/wedget/wedget.dart
+++ lib/screens/wedget/wedget.dart
@@ -4,7 +4,7 @@ import 'package:flutter_foodi_app/model/prodact_view.dart';
 final alldata = product;
 onbordcard(BuildContext context, String img) {
   final screenhight = MediaQuery.of(context).size.height;
-  final screenwidth = MediaQuery.of(context).size.width;
+
   return Container(
     width: MediaQuery.of(context).size.width,
     height: screenhight * 0.5 + 34,
@@ -72,14 +72,16 @@ onbordbutton(String text) {
   );
 }
 
-foodcard(String img, String txt,String tx) {
+foodcard(String img, String txt, String tx, BuildContext context) {
+  final screenwidth = MediaQuery.of(context).size.width;
   return Row(
     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    
     children: [
       Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
+        padding: const EdgeInsets.symmetric(horizontal: 22),
         child: Container(
-          width: 152,
+          width: screenwidth * 0.39,
           height: 183,
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(16),
@@ -101,14 +103,22 @@ foodcard(String img, String txt,String tx) {
                   top: 16,
                   right: 20,
                 ),
-                child:
-                    Container(child: Image.asset('assets/images/Vector.png')),
+                child: Container(
+                  height: 16,
+                  width: 15,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/Vector.png'))),
+                ),
               ),
               Center(
                 child: Container(
                   width: 80,
                   height: 80,
-                  child: Image.asset(img),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage(img))
+                  ),
+                 
                 ),
               ),
               SizedBox(
@@ -163,4 +173,99 @@ foodcard(String img, String txt,String tx) {
       ),
     ],
   );
+} rowfoodcard(String img, String txt, String tx, BuildContext context, int clor){
+ 
+return Container(
+  width: 163,
+  height: 170,
+  decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      color: Color(clor),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.1), // soft shadow
+          spreadRadius: 0,
+          blurRadius: 1,
+          offset: const Offset(0, 3),
+        )
+      ]),
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      Padding(
+        padding: const EdgeInsets.only(
+          top: 16,
+          right: 20,
+        ),
+        child: Container(
+          height: 16,
+          width: 15,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/Vector.png'))),
+        ),
+      ),
+      Center(
+        child: Container(
+          width: 64,
+          height: 64,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage(img))
+          ),
+         
+        ),
+      ),
+      SizedBox(
+        height: 8,
+      ),
+      Center(
+        child: Text(txt,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            )),
+      ),
+      SizedBox(
+        height: 6,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 16,
+                  height: 12.800000190734863,
+                  child: Image.asset('assets/images/price.png'),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(tx,
+                    style: TextStyle(
+                      color: Color(0xffF08626),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 15),
+            child: Container(
+              
+              child: Image.asset('assets/images/add_icon.png',fit: BoxFit.cover,),
+            ),
+          ),
+        ],
+      )
+    ],
+  ),
+);
+
+
 }
